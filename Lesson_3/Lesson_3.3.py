cc0dbc7d75fbc713fb6121ac3cf262f75ee31130
MrBerwald <mrberwald@gmail.com>
2021-07-10 20:48:17 +0200

    Homework 3.3

--- Lesson_3/Lesson_3.3.py
+++ Lesson_3/Lesson_3.3.py
@@ -5,9 +5,9 @@
 def my_func(arg1, arg2, arg3):
     if arg1>=arg3 and arg2>=arg3:
         return arg1 + arg2
-    elif arg1<arg3 and arg2<arg3:
-        return arg1 + arg3
-    else:
+    elif arg2>=arg1 and arg3>=arg1:
         return arg2 + arg3
+    else:
+        return arg1 + arg3
 
 print(f'Результат - {my_func(int(input("Введите первое число: ")), int(input("Введите второе число: ")), int(input("Введите третье число: ")))}')
